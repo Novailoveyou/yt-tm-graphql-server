ca51877a1c90a20d0006a236590a9cd3694067a2
Novailoveyou <novailoveyoux@gmail.com>
2021-12-14 16:41:42 +0300

    rootQuery & fetching data

--- schema.js
+++ schema.js
@@ -1,9 +1,69 @@
-import { buildSchema } from 'graphql'
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} from 'graphql'
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
+// Hardcoded data
+const customers = [
+  {
+    id: '1',
+    name: 'John Doe',
+    email: 'jdoe@gmail.com',
+    age: 35
+  },
+  {
+    id: '2',
+    name: 'Steve Smith',
+    email: 'steeve@gmail.com',
+    age: 25
+  },
+  {
+    id: '3',
+    name: 'Sara Williams',
+    email: 'sara@gmail.com',
+    age: 32
   }
-`)
+]
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return customers.find(customer => customer.id === args.id)
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers
+      }
+    }
+  }
+})
+
+const schema = new GraphQLSchema({
+  query: RootQuery
+})
 
 export default schema
